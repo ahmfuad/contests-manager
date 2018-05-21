21a68fab93fb887ed12b08015f61cb12b841cb7d
Romchik908 <romchik908@gmail.com>
2018-05-21 15:15:53 +0500

    some

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var path = require('path');
 let nconf = require("nconf");
 let express = require("express");
     let app = express();
@@ -6,6 +7,15 @@ nconf.argv()
     .env()
     .file({ file: './config.json' });
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.sendfile('./views/layout.html');
+});
+
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 global.__basedir = __dirname;
 global.__databaseUri = process.argv.indexOf("--local") > -1 ? nconf.get("database:uri-local") : nconf.get("database:uri");
